af85e028ef0864fc26b597c14f28dace01575d83
zeromin41 <tladudals224@hanmail.net>
2023-12-05 22:10:19 +0900

    방해꾼 수정

--- Assets/Scripts/Disruptor.cs
+++ Assets/Scripts/Disruptor.cs
@@ -5,16 +5,16 @@ using UnityEngine;
 public class Disruptor : MonoBehaviour
 {
     public float moveSpeed = 2f; // 이동 속도
-    private Vector2[] moveDirections = { Vector2.up, Vector2.down, Vector2.left, Vector2.right }; // 상하좌우 이동 방향
-    private int currentDirectionIndex = 0; // 현재 이동 방향의 인덱스
+    private Vector2[] moveDirections = { Vector2.up, Vector2.down }; // 상하 이동 방향
     private Vector2 currentTarget; // 현재 목표 위치
     [SerializeField]
-    Vector2 startPos;       //시작 위치
+    Vector2 startPos; // 시작 위치
+    private bool moveUp = true; // 현재는 위로 이동 중인지 여부
+
     void Start()
     {
         // 초기 위치 설정
-
-        transform.position = startPos;     
+        transform.position = startPos;
         SetNewTarget();
     }
 
@@ -23,39 +23,33 @@ public class Disruptor : MonoBehaviour
         // 목표 위치로 이동
         transform.position = Vector2.MoveTowards(transform.position, currentTarget, moveSpeed * Time.deltaTime);
 
-        // 목표 위치에 도착했을 때 새로운 랜덤 방향과 목표 위치 설정
+        // 목표 위치에 도착했을 때 새로운 목표 위치 설정
         if ((Vector2)transform.position == currentTarget)
         {
-            SetRandomDirection();
-            SetNewTarget();
+            if (moveUp)
+                SetNewTargetUp();
+            else
+                SetNewTargetDown();
         }
     }
 
-    void SetRandomDirection()
+    void SetNewTargetUp()
     {
-        // 랜덤한 이동 방향 선택
-        currentDirectionIndex = Random.Range(0, moveDirections.Length);
+        currentTarget = (Vector2)transform.position + Vector2.up;
+        moveUp = false;
+    }
 
-        // 현재 방향이 맵 가장자리로 이동하는지 체크
-        CheckEdge();
+    void SetNewTargetDown()
+    {
+        currentTarget = (Vector2)transform.position + Vector2.down;
+        moveUp = true;
     }
 
     void SetNewTarget()
     {
-        // 현재 위치에서 상하좌우 중 랜덤 방향으로 이동할 위치 설정
-        currentTarget = (Vector2)transform.position + moveDirections[currentDirectionIndex];
-
-        // 현재 방향이 맵 가장자리로 이동하는지 체크
-        CheckEdge();
-    }
-
-    void CheckEdge()
-    {
-        // 현재 위치가 맵 가장자리에 도달하면 방향을 반대로 설정
-        if (transform.position.x <= -8f || transform.position.x >= 8f || transform.position.y <= -4.5f || transform.position.y >= 4.5f)
-        {
-            // 현재 방향을 반대로 설정
-            currentDirectionIndex = (currentDirectionIndex + 2) % moveDirections.Length;
-        }
+        if (moveUp)
+            SetNewTargetUp();
+        else
+            SetNewTargetDown();
     }
 }
