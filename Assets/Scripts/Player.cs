9bb19de786a49135e95d69f4b9ad1d1a3b883067
zeromin41 <zeromin41>
2024-01-08 19:19:14 +0900

    무적효과 추가 및 씬추가

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,7 +2,8 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Player : MonoBehaviour {
+public class Player : MonoBehaviour
+{
     float gridValue;
     [SerializeField] float maxPos = 3.6f;
     [SerializeField] Vector2 playerPos;
@@ -10,31 +11,38 @@ public class Player : MonoBehaviour {
     private bool isInvincible = false;
     public float invincibilityDuration = 3f;
 
-    void Start() {
+    void Start()
+    {
 #if UNITY_EDITOR
         gridValue = UnityEditor.EditorSnapSettings.move.x;
 #endif
     }
 
-    void Update() {
+    void Update()
+    {
         Mover();
     }
 
-    void Mover() {
+    void Mover()
+    {
         Vector3 moveDirection = Vector3.zero;
-        if (Input.GetKeyDown(KeyCode.A)) {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
             moveDirection += Vector3.left * gridValue;
             transform.localEulerAngles = new Vector3(0, 0, 0);
         }
-        if (Input.GetKeyDown(KeyCode.D)) {
+        if (Input.GetKeyDown(KeyCode.D))
+        {
             moveDirection += Vector3.right * gridValue;
             transform.localEulerAngles = new Vector3(0, 0, 180);
         }
-        if (Input.GetKeyDown(KeyCode.S)) {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
             moveDirection += Vector3.down * gridValue;
             transform.localEulerAngles = new Vector3(0, 0, 90);
         }
-        if (Input.GetKeyDown(KeyCode.W)) {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
             moveDirection += Vector3.up * gridValue;
             transform.localEulerAngles = new Vector3(0, 0, -90);
         }
@@ -45,37 +53,55 @@ public class Player : MonoBehaviour {
         transform.position = newPosition;
     }
 
-    void OnTriggerEnter2D(Collider2D collision) {
-        if (collision.CompareTag("Bomb")) {
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Bomb"))
+        {
             Debug.Log("지뢰 밟음");
             Debug.Log("Game Over");
             SceneManager.LoadScene(0);
-        } else if (collision.CompareTag("InvincibilityItem")) {
+        }
+        else if (collision.CompareTag("InvincibilityItem"))
+        {
             Debug.Log("아이템 사용");
-            if (isInvincible) {
+            if (isInvincible)
+            {
                 Debug.Log("이미 무적 상태입니다.");
-            } else {
+            }
+            else
+            {
                 StartCoroutine(ActivateInvincibility());
             }
         }
     }
 
-    private IEnumerator ActivateInvincibility() {
+    private IEnumerator ActivateInvincibility()
+    {
         isInvincible = true;
         Debug.Log("무적 상태로 전환");
 
-        // 무적상태일때 처리코드
+        // 플레이어 색상을 초록색으로 변경
+        GetComponent<SpriteRenderer>().color = Color.green;
+
+        // 무적 상태일 때 처리코드
 
         yield return new WaitForSeconds(invincibilityDuration);
 
         isInvincible = false;
         Debug.Log("무적 상태 해제");
+
+        // 플레이어 색상을 다시 흰색으로 변경
+        GetComponent<SpriteRenderer>().color = Color.white;
     }
 
-    public void OnPlayerCollision(Collider2D disruptorCollider) {
-        if (isInvincible) {
+    public void OnPlayerCollision(Collider2D disruptorCollider)
+    {
+        if (isInvincible)
+        {
             Debug.Log("무적 상태로 부딪히지 않음");
-        } else {
+        }
+        else
+        {
             Debug.Log("방해꾼 부딪혔음");
             SceneManager.LoadScene(0);
             // 부딪쳤을때 처리코드
