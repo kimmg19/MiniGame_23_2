3e3ac1d8bd0de11b7ae671c643a9e3cba39277c7
qweasd122 <149946775+qweasd122@users.noreply.github.com>
2024-01-30 15:11:08 +0900

    Add files via upload

--- Assets/Scripts/BtnControl.cs
+++ Assets/Scripts/BtnControl.cs
@@ -37,6 +37,56 @@ public class BtnControl : MonoBehaviour
         SceneManager.LoadScene("Level 1");
     }
 
+    public void OnclickStage2Btn()
+    {
+        SceneManager.LoadScene("Level 2");
+    }
+
+    public void OnclickStage3Btn()
+    {
+        SceneManager.LoadScene("Level 3");
+    }
+
+    public void OnclickStage4Btn()
+    {
+        SceneManager.LoadScene("Level 4");
+    }
+
+    public void OnclickStage5Btn()
+    {
+        SceneManager.LoadScene("Level 5");
+    }
+
+    public void OnclickStage6Btn()
+    {
+        SceneManager.LoadScene("Level 6");
+    }
+
+    public void OnclickStage7Btn()
+    {
+        SceneManager.LoadScene("Level 7");
+    }
+
+    public void OnclickStage8Btn()
+    {
+        SceneManager.LoadScene("Level 8");
+    }
+
+    public void OnclickStage9Btn()
+    {
+        SceneManager.LoadScene("Level 9");
+    }
+
+    public void OnclickStage10Btn()
+    {
+        SceneManager.LoadScene("Level 10");
+    }
+
+    public void OnclickStage11Btn()
+    {
+        SceneManager.LoadScene("Level 11");
+    }
+
     public void OnclickPauseBtn()
     {
         Time.timeScale = 0;
